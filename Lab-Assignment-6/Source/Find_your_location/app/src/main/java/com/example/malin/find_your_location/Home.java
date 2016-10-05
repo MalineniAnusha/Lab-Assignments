8ca3df74f120691d0393f810926b5b558b3988f0
MalineniAnusha <malinenianusha@gmail.com>
2016-10-05 15:18:07 -0500

    commit for source code

---
+++ Lab-Assignment-6/Source/Find_your_location/app/src/main/java/com/example/malin/find_your_location/Home.java
@@ -0,0 +1,156 @@
+package com.example.malin.find_your_location;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+/**
+ * Created by malin on 05-10-2016.
+ */
+
+public class Home extends AppCompatActivity {
+
+    private GoogleMap mMap;
+    public Geocoder geocoder;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps);
+        // getActionBar().setTitle("Map location");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        geocoder = new Geocoder(this);
+        StringBuilder userAddress = new StringBuilder();
+        // Add a marker in Sydney and move the camera
+        LocationManager userCurrentLocation = (LocationManager) this
+                .getSystemService(Context.LOCATION_SERVICE);
+        LocationListener userCurrentLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        LatLng userCurrentLocationCorodinates = null;
+        double latitute = 0, longitude = 0;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //show message or ask permissions from the user.
+            return;
+        }
+        //Getting the current location of the user.
+        userCurrentLocation.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0, 0, userCurrentLocationListener);
+        latitute = userCurrentLocation
+                .getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                .getLatitude();
+        longitude = userCurrentLocation
+                .getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                .getLongitude();
+        userCurrentLocationCorodinates = new LatLng(latitute, longitude);
+        //Getting the address of the user based on latitude and longitude.
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitute, longitude, 1);
+            Address address = addresses.get(0);
+            userAddress = new StringBuilder();
+            for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                userAddress.append(address.getAddressLine(i)).append("\t");
+            }
+            userAddress.append(address.getCountryName()).append("\t");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        //Setting our image as the marker icon.
+        mMap.addMarker(new MarkerOptions().position(userCurrentLocationCorodinates)
+                .title("Your current address.").snippet(userAddress.toString())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_maps)));
+        //Setting the zoom level of the map.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocationCorodinates, 7));
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        NotificationCompat.Action viewAction = NotificationCompat.Action.newAction(
+                NotificationCompat.Action.TYPE_VIEW, // TODO: choose an action type.
+                "Home Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.malin.lab_assignment_6/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        NotificationCompat.Action viewAction = NotificationCompat.Action.newAction(
+                NotificationCompat.Action.TYPE_VIEW, // TODO: choose an action type.
+                "Home Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.malin.lab_assignment_6/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
